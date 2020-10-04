cb1b840169f04d2c7eea9911f447b60ac01328a1
Joshua Sevy <joshuasevy@outlook.com>
2020-10-03 22:58:57 -0600

    Oauth signs in and out no bugs

--- Client/src/components/OAuth/GoogleAuth.js
+++ Client/src/components/OAuth/GoogleAuth.js
@@ -19,7 +19,7 @@ class GoogleAuth extends Component {
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.onAuthChange();
-          this.auth.isSignedIn.Listen(this.onAuthChange)
+          this.auth.isSignedIn.listen(this.onAuthChange)
         });
     });
   }
@@ -28,19 +28,32 @@ class GoogleAuth extends Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() })
   };
 
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button className="ui red google button">
+        <button 
+          className="ui red google button"
+          onClick={this.onSignOut}
+        >
           <i className="google icon" />
           Sign Out
         </button>
       )
     } else {
       return (
-        <button className="ui red google button">
+        <button 
+          className="ui red google button" onClick={this.onSignIn}
+        >
           <i className="google icon" />
           Sign In with Google
         </button>
